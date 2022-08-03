7c61cc816554b556f58b0c7756a7072490a48e7c
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-08-03 09:50:55 +0200

    created util for getting channel from database

---
+++ src/utils/GetChannel.ts
@@ -0,0 +1,23 @@
+import { Channel, Client, TextChannel } from "discord.js";
+import { db } from "../prisma/Client";
+
+export default async function GetChannel(
+  client: Client,
+  channelName: string,
+  serverId: string
+): Promise<Channel | undefined> {
+  const channel = await db.channels.findFirst({
+    where: {
+      channelName: { equals: channelName },
+      serverId: { equals: serverId },
+    },
+  });
+  if (channel != null) {
+    const FetchedChannel = await client.channels.fetch(channel.channelId);
+    if (FetchedChannel) {
+      return FetchedChannel;
+    }
+  } else {
+    throw new Error("Channel not found");
+  }
+}
