6a0a1caa8fe0d21a58174b37a49f1153ad6c1d5b
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-08-03 10:40:47 +0200

    added channelname command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,5 +1,6 @@
 import { SetChannel } from "./commands/SetChannel";
 import { Warn } from "./commands/Warn";
 import { Command } from "./types/Command";
+import { ChannelName } from "./commands/ChannelName";
 
-export const Commands: Command[] = [Warn, SetChannel];
+export const Commands: Command[] = [Warn, SetChannel, ChannelName];
---
+++ src/commands/ChannelName.ts
@@ -0,0 +1,75 @@
+import {
+  CommandInteraction,
+  Client,
+  ApplicationCommandType,
+  ApplicationCommandOptionType,
+  PermissionFlagsBits,
+} from "discord.js";
+import { Command } from "../types/Command";
+
+const nameDic = {
+  a: "ᴀ",
+  b: "ʙ",
+  c: "ᴄ",
+  d: "ᴅ",
+  e: "ᴇ",
+  f: "ꜰ",
+  g: "ɢ",
+  h: "ʜ",
+  i: "ɪ",
+  j: "ᴊ",
+  k: "ᴋ",
+  l: "ʟ",
+  m: "ᴍ",
+  n: "ɴ",
+  o: "ᴏ",
+  p: "ᴘ",
+  q: "Q",
+  r: "ʀ",
+  s: "ꜱ",
+  t: "ᴛ",
+  u: "ᴜ",
+  v: "ᴠ",
+  w: "ᴡ",
+  x: "x",
+  y: "ʏ",
+  z: "ᴢ",
+  "-": "-",
+  "0": "0",
+  "1": "1",
+  "2": "2",
+  "3": "3",
+  "4": "4",
+  "5": "5",
+  "6": "6",
+  "7": "7",
+  "8": "8",
+  "9": "9",
+  " ": "-",
+};
+
+export const ChannelName: Command = {
+  name: "channelname",
+  description: "Returns the channel name using the font used in the server",
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: "channelname",
+      description: "Provide the name of the channel",
+      required: true,
+    },
+  ],
+  defaultMemberPermissions: PermissionFlagsBits.ManageGuild,
+  run: async (client: Client, interaction: CommandInteraction) => {
+    let returnValue: string = "";
+    if (interaction.options.data[0].value) {
+      const channelName = interaction.options.data[0].value.toString();
+      for (const char of channelName) {
+        let index = Object.keys(nameDic).indexOf(char);
+        returnValue += Object.values(nameDic)[index];
+      }
+      await interaction.followUp({ content: "︱" + returnValue });
+    }
+  },
+};
