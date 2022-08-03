53e871178933bcf256686f0481e5b1de31e28623
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-08-03 09:50:26 +0200

    created command for setting channels

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,3 +1,5 @@
+import { SetChannel } from "./commands/SetChannel";
+import { Warn } from "./commands/Warn";
 import { Command } from "./types/Command";
 
-export const Commands: Command[] = [];
+export const Commands: Command[] = [Warn, SetChannel];
---
+++ src/commands/SetChannel.ts
@@ -0,0 +1,59 @@
+import {
+  CommandInteraction,
+  Client,
+  ApplicationCommandType,
+  ApplicationCommandOptionType,
+} from "discord.js";
+import { Command } from "../types/Command";
+import { db } from "../prisma/Client";
+import { log } from "console";
+
+export const SetChannel: Command = {
+  name: "setchannel",
+  description: "Sets channel id for different channels the bot uses",
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: "channelname",
+      description: "Select which channel do you want to set up",
+      choices: [{ name: "Warnings Channel", value: "warningsChannel" }],
+      required: true,
+    },
+    {
+      type: ApplicationCommandOptionType.Channel,
+      name: "channel",
+      description: "Select the channel you want to use",
+      required: true,
+    },
+  ],
+  run: async (client: Client, interaction: CommandInteraction) => {
+    if (
+      interaction.options.data[0].value &&
+      interaction.options.data[1].channel?.id &&
+      interaction.guild
+    ) {
+      await db.channels.upsert({
+        where: {
+          channelName: interaction.options.data[0].value + interaction.guild.id,
+        },
+        update: { channelId: interaction.options.data[1].channel.id },
+        create: {
+          channelName:
+            interaction.options.data[0].value.toString() + interaction.guild.id,
+          channelId: interaction.options.data[1].channel.id,
+          serverId: interaction.guild.id,
+        },
+      });
+      await interaction.followUp({
+        content: `Set the ${interaction.options.data[0].value.toString()} to <#${
+          interaction.options.data[1].channel.id
+        }>`,
+      });
+    } else {
+      await interaction.followUp({
+        content: "There was an error. Please try again",
+      });
+    }
+  },
+};
