b1c8a53eea8ea3587fc849d5219c5019e131cfa1
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-08-23 13:34:01 +0200

    fix: used guildId instead of user's id

--- src/listeners/on_join.ts
+++ src/listeners/on_join.ts
@@ -15,12 +15,12 @@ export default (client: Client): void => {
       switch (member.guild.id) {
         case "774604623247573022":
           await welcomeChannel.send({
-            content: `**Welcome <@${member.guild.id}>**\n\nPlease use this chat if you have any questions and someone will be on hand.\n\nAll the information you need is on <#865379267977412618>`,
+            content: `**Welcome <@${member.id}>**\n\nPlease use this chat if you have any questions and someone will be on hand.\n\nAll the information you need is on <#865379267977412618>`,
           });
           break;
         case "935253214285135952":
           await welcomeChannel.send({
-            content: `Welcome to F2ABEEZ!<@${member.guild.id}>\n\nYour dedicated F2 racing discord community. Please read <#937998062842957824> to get equated with our brand and information then, head over to <#937997355737833482> to get a seat in the next race that suits your pace! Please **put your gamertag into brackets behind your discord name**, thank you!`,
+            content: `Welcome to F2ABEEZ!<@${member.id}>\n\nYour dedicated F2 racing discord community. Please read <#937998062842957824> to get equated with our brand and information then, head over to <#937997355737833482> to get a seat in the next race that suits your pace! Please **put your gamertag into brackets behind your discord name**, thank you!`,
           });
         default:
           break;
