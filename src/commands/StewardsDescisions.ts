4957775babe5734d190f490daef122975e05ad58
Jakub Hašek <28836407+kubahasek@users.noreply.github.com>
2022-12-12 20:12:59 +0100

    edited stewards annoucement

--- src/commands/StewardsDescisions.ts
+++ src/commands/StewardsDescisions.ts
@@ -52,15 +52,15 @@ export const StewardsDescisions: Command = {
             );
             const tier1URL = `<https://f1abeez.com/race-reports/t1/r${round}>`;
             const tier2URL = `<https://f1abeez.com/race-reports/t2/r${round}>`;
-            const tier3URL = `<https://f1abeez.com/race-reports/t3/r${round}>`;
+            // const tier3URL = `<https://f1abeez.com/race-reports/t3/r${round}>`;
             // const tier4URL = `<https://f1abeez.com/race-reports/t4/${round}>`;
-            const tierHURL = `<https://f1abeez.com/race-reports/th/r${round}>`;
-            const tierMURL = `<https://f1abeez.com/race-reports/tm/r${round}>`;
+            // const tierHURL = `<https://f1abeez.com/race-reports/th/r${round}>`;
+            // const tierMURL = `<https://f1abeez.com/race-reports/tm/r${round}>`;
             // const f2Tier1URL = `https://f1abeez.com/race-reports/f2/t1/${round}>`;
             const bansURL = `https://www.f1abeez.com/bans-of-the-week`;
             if (channel && channel.isTextBased()) {
               await channel.send({
-                content: `🦺 @everyone\n\n**All Stewards decisions are finalised**\nPlease check this week's race-report for all the incidents reported and decisions made.\n\n**F1 - Tier 1** - ${tier1URL}\n**F1 - Tier 2** - ${tier2URL}\n**F1 - Tier 3** - ${tier3URL}\n**F1 - Tier H** - ${tierHURL}\n**F1 - Tier M** - ${tierMURL}\n**Bans Issued for next round: **${bansURL}\n\nPlease file your appeals with the correct case number **in the next 24 hours**, and standings will be posted after all appeals are finalised \nFollow the instructions in <#864999507238322186> to submit your appeals \n\nThank you,\nStewards of F1ABEEZ`,
+                content: `🦺 @everyone\n\n**All Stewards decisions are finalised**\nPlease check this week's race-report for all the incidents reported and decisions made.\n\n**F1 - Tier 1** - ${tier1URL}\n**F1 - Tier 2** - ${tier2URL}\n**Bans Issued for next round: **${bansURL}\n\nPlease file your appeals with the correct case number **in the next 24 hours**, and standings will be posted after all appeals are finalised \nFollow the instructions in <#864999507238322186> to submit your appeals \n\nThank you,\nStewards of F1ABEEZ`,
               });
               await interaction.followUp({
                 content: `Message sent in <#${channel.id}>`,
